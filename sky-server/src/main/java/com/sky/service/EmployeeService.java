82dcdd5f298c731695e4da87a768922b39cb4f99
chuan <hello@chuan.cn>
2025-06-10 20:36:30 +0800

    分类管理模块功能代码

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -37,4 +37,14 @@ public interface EmployeeService {
      * @param id
      */
     void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
+
+    Employee getById(Long id);
+
+    void update(EmployeeDTO employeeDTO);
 }
